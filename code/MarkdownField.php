38ce007acbda3b01c93ba0f2fdf53b64d089012c
Wolf Vollprecht <w.vollprecht@gmail.com>
2012-02-01 01:04:24 +0100

    Switched to ACE - Editor, improvements in Image & Link adding

--- code/MarkdownField.php
+++ code/MarkdownField.php
@@ -2,18 +2,21 @@
 
 class MarkdownField extends TextareaField {
 	protected $template = 'MarkdownField';
-
+	static $editortheme = 'twilight';
 	function Field($properties = array()) {
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-		Requirements::javascript('markdown/javascript/showdown.js');
-		Requirements::javascript('markdown/javascript/md.js');
-		Requirements::themedCSS('typography');
+		Requirements::javascript('markdown/javascript/markdownField.js');
 		Requirements::css('markdown/css/md.css');
+
 		if($this->disabled) $attributes['disabled'] = 'disabled';
 		$properties["class"] = "markdown_Input";
+
 		$properties["LinkSuggest"] = $this->Link() . '/linksuggest';
 		$properties["ImageSuggest"] = $this->Link() . '/imagesuggest';
 		$properties["FindLink"] = $this->Link() . '/getlinktoid';
+
+		$properties["Editor"] = $this->Link() . '/getEditor';
+
 		$obj = ($properties) ? $this->customise($properties) : $this;
 		return $obj->renderWith($this->getTemplate());
 	}
@@ -22,15 +25,47 @@ class MarkdownField extends TextareaField {
 		$search = $this->request["search"];
 		$data = DataList::create('SiteTree')->filter(array("Title:StartsWith" => $search));
 		$data_array = $data->toNestedArray();
-
 		return json_encode($data_array);
 	}
 
 	function getlinktoid() {
 		$id = $this->request["ID"];
 		$data = DataList::create('SiteTree')->byID($id);
-
 		return $data->Link();
 	}
 
+	function imagesuggest() {
+		$search = $this->request["search"];
+		$data = DataList::create('File')->filter(array("Title:PartialMatch" => $search));
+		$data_array = $data->toNestedArray();
+		return json_encode($data_array);
+	}
+
+	function getimagelinktoid() {
+		$id = $this->request["ID"];
+		$data = DataList::create('File')->byID($id);
+		return $data->Link();
+	}
+
+	public function getEditor() {
+		Requirements::clear();
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery-ui/jquery-ui.js');
+		Requirements::javascript(THIRDPARTY_DIR. '/jquery-entwine/dist/jquery.entwine-dist.js');
+		Requirements::javascript('markdown/javascript/lib/showdown.js');
+		Requirements::javascript('markdown/javascript/lib/ace/src/ace.js');
+		Requirements::javascript('markdown/javascript/lib/ace/src/mode-markdown.js');
+		Requirements::javascript('markdown/javascript/lib/ace/src/theme-' . self::$editortheme . '.js');
+		Requirements::javascript('markdown/javascript/markdownEditor.js');
+		Requirements::themedCSS('typography');
+		Requirements::css('markdown/css/md.css');
+		//Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery-ui.css');
+
+		$properties["LinkSuggest"] = 'linksuggest';
+		$properties["ImageSuggest"] = 'imagesuggest';
+		$properties["FindLink"] = 'getlinktoid';
+		$properties["FindImageLink"] = 'getimagelinktoid';
+
+		return $this->customise($properties)->renderWith('MarkdownEditor');
+	}
 }
--- templates/forms/MarkdownField.ss
+++ templates/forms/MarkdownField.ss
@@ -1,8 +1,3 @@
-<textarea $AttributesHTML linksuggest="$LinkSuggest" findlink="$FindLink" imagesuggest="$ImageSuggest">$Value</textarea>
-<div class="typography markdown_preview"></div>
-
-<div class="markdown_helper">
-	<input id="markdown_AutoSuggestLinks" />
-	<div class="markdown_CaretHelper markdown"></div>
-</div>
+<textarea $AttributesHTML>$Value</textarea>
+<iframe class="markdown_Iframe" src="$Editor"></iframe>
 <div class="clear"></div>
